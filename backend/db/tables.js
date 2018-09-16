13ad5fd5ef2a05b4de277188fb1213a65a89db76
SchaeStewart <SchaeStewart@gmail.com>
2018-09-15 21:36:53 -0400

    Init docker

--- backend/db/tables.js
+++ backend/db/tables.js
@@ -2,4 +2,6 @@ const db = require('../db')
 
 db.query('CREATE TABLE questions(id SERIAL PRIMARY KEY, postTitle VARCHAR(255) not null, permalink VARCHAR(255) not null, url VARCHAR(255) not null, theOnion BOOLEAN not null)')
     .then(res => { console.log(res) })
-    .catch( err => { console.log(err) })
+    .catch( err => {
+        console.log(err, 'error creating tables')
+    })
