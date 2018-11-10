34c9ba2362ebb761d349edf2f729eb7b772a54d3
Schaffer <SchaeStewart@gmail.com>
2018-11-09 21:11:12 -0500

    Add About and HowToPlay content

--- ui/src/containers/About.js
+++ ui/src/containers/About.js
@@ -1,5 +1,21 @@
 import React from 'react'
+import { Box, Text, Heading } from 'grommet'
 
-const About = () => <div>Info about me and project. Link to github</div>
+const About = () => (
+  <Box align="center">
+    <header>
+      <Heading level={1} size="medium" color="brand">
+        About
+      </Heading>
+    </header>
+    <Text align>
+      This project was built by Schaffer Stewart for learning and fun. <br />
+      It is open source and built with AWS Lambda, Serverless and React <br />
+      <a href="https://github.com/SchaeStewart/OnionOrNot">
+        See the project on GitHub
+      </a>
+    </Text>
+  </Box>
+)
 
 export default About
