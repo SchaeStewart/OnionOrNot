ed227c040716aac1bbc8a5358a9b310c30e078d6
Schae Stewart <schaestewart@gmail.com>
2019-05-22 13:22:01 -0400

    Style Answer.svelte

--- ui-svelte/src/Answer.svelte
+++ ui-svelte/src/Answer.svelte
@@ -1,6 +1,10 @@
 <script>
   export let answer;
   export let newRoundHandler;
+
+  $: titleClass = `title has-text-${
+    answer && answer.correct ? "success" : "danger"
+  }`;
 </script>
 
 <style>
@@ -13,11 +17,13 @@
 </style>
 
 {#if answer !== null}
-  <p>{answer.correct ? 'You got it right!' : 'You got it wrong'}</p>
+  <p class={titleClass}> {answer.correct ? 'Correct!' : 'Wrong!'} </p>
   <p>
     <a href={`https://old.reddit.com/${answer.permalink}`}>View Reddit Post</a>
   </p>
   <a href={answer.url}>View Article</a>
   <br />
-  <button on:click={newRoundHandler}>Get a new question</button>
+  <button class="button is-primary" on:click={newRoundHandler}>
+    Get a new question
+  </button>
 {/if}
